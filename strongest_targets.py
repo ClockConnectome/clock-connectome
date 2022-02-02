d556947c03827ffb0333928144e0cc4dad198d81
daphnaspira <des2188@barnard.edu>
2022-02-01 20:11:07 -0500

    updates based on the new version of the colab notebook & gabrielle's notes

--- strongest_targets.py
+++ strongest_targets.py
@@ -1,29 +1,20 @@
-def get_strong_shared_targs(IDs):
-    """
-    Gets strong shared targets of specified bodyIds for the neuron type
-    :param IDs: list of bodyIds
-    :return:
-    """
-    from neuprint import fetch_simple_connections
-    
-    test = fetch_simple_connections(IDs, None)
-    test['shared'] = 0
-    post_IDs = set(test['bodyId_post'])
+def get_strong_shared_targs(IDs, shared_num):
+  from neuprint import fetch_simple_connections
 
-    for p in post_IDs:
-        pre_list = test.loc[test['bodyId_post']==p, 'bodyId_pre']
-        if len(pre_list) > 3:
-            test.loc[test['bodyId_post']==p, 'shared'] = 1
+  test = fetch_simple_connections(IDs, None, min_weight=10)
+  test['shared'] = 1
       
-    test = test.groupby(['bodyId_post'], as_index=False)['weight'].sum()
-    test = test.sort_values(by=['weight'], ascending=False)
+  test = test.groupby(['bodyId_post'], as_index=False)['weight','shared'].sum()
+  test = test.sort_values(by=['weight'], ascending=False)
+  test = test.loc[test['shared']==shared_num]
 
-    return test
+  return test
 
-def strongest_shared_targets(candidate_IDs, direction):
+def get_input_output_conns(IDs, strength, direction):
     """
     Retrieves data for candidate neuron inputs or outputs and returns them sorted by weight
     :param candidate_IDs: the bodyIds of the neurons of interest
+    :param strength: (int) minimum connection strength
     :param direction: (string) specified connection direction to run:
         'in' for inputs to clock neurons from anything else, 
         'out' for outputs from clock neurons to anything else.
@@ -31,11 +22,11 @@ def strongest_shared_targets(candidate_IDs, direction):
     """
     from neuprint import fetch_simple_connections
     if direction == 'in':
-        candidate_conns = fetch_simple_connections(None, candidate_IDs)
-        candidate_conns = candidate_conns[['bodyId_post','bodyId_pre','instance_pre','weight']]
+        candidate_conns = fetch_simple_connections(None, IDs, min_weight=strength)
+        candidate_conns = candidate_conns[['bodyId_post','instance_post','bodyId_pre','instance_pre','weight']]
         candidate_conns = candidate_conns.sort_values(by=['bodyId_post','weight'], ascending = False)
     if direction == 'out':
-        candidate_conns = fetch_simple_connections(candidate_IDs, None)
-        candidate_conns = candidate_conns[['bodyId_pre','bodyId_post','instance_post','weight']]
+        candidate_conns = fetch_simple_connections(IDs, None, min_weight=strength)
+        candidate_conns = candidate_conns[['bodyId_pre','instance_pre','bodyId_post','instance_post','weight']]
         candidate_conns = candidate_conns.sort_values(by=['bodyId_pre','weight'], ascending = False)
     return candidate_conns
